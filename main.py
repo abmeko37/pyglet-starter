79e74fb5851c91ac9c047f24e813d17633d78750
Abrahim <60404056+abmeko37@users.noreply.github.com>
2020-02-11 13:33:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,14 @@
-<<<<<<< Updated upstream
-import pyglet # import the library
-window = pyglet.window.Window() # create the window
-
-# Create some text
-# label = pyglet.text.Label('Hello, world', x = 200, y = 200)
-
-# Create a sprite
-ball_image = pyglet.image.load('assets/hero/Old hero.png')
-ball = pyglet.sprite.Sprite(ball_image, x=50, y=50)
-
-# Start the event loop
-=======
 import pyglet
 window = pyglet.window.Window()
 
-label = pyglet.text.Label('there is something wrong with this computer',
+label = pyglet.text.Label('there is somethingfad wrong with this computer',
                           font_name='Times New Roman',
                           font_size=30,
                           x=window.width//2, y=window.height//2,
-                          anchor_x='5', anchor_y='6')
->>>>>>> Stashed changes
+                          anchor_x='center', anchor_y='center')
 @window.event
 def on_draw():
     window.clear()
-    ball.x += 1
-    ball.draw()
+    label.draw()
 
 pyglet.app.run()
