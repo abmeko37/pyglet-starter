c99b2176c023a4f7b6c2f43618f8d6832695b88d
Abrahim <60404056+abmeko37@users.noreply.github.com>
2020-02-11 13:31:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import pyglet # import the library
 window = pyglet.window.Window() # create the window
 
@@ -9,6 +10,16 @@ ball_image = pyglet.image.load('assets/hero/Old hero.png')
 ball = pyglet.sprite.Sprite(ball_image, x=50, y=50)
 
 # Start the event loop
+=======
+import pyglet
+window = pyglet.window.Window()
+
+label = pyglet.text.Label('there is something wrong with this computer',
+                          font_name='Times New Roman',
+                          font_size=30,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='5', anchor_y='6')
+>>>>>>> Stashed changes
 @window.event
 def on_draw():
     window.clear()
