06f0c0ad476a7820c220a0971f7965287ac9d44a
Abrahim <60404056+abmeko37@users.noreply.github.com>
2020-02-11 14:31:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import pygame
 import pyglet
 window = pyglet.window.Window()
+def update (dt):
+    pass
 
-label = pyglet.text.Label('there is somethingfad wrong with this computer',
-                          font_name='Times New Roman',
-                          font_size=30,
-                          x=window.width//2, y=window.height//2,
-                          anchor_x='center', anchor_y='center')
+
+
+image = pyglet.image.load('assets/hero/sliced/idle-1.png')
+cavimg = pyglet.image.load('assets/forest-assets/cave.png')
+floor = pyglet.image.load('assets/forest-assets/floor.png')
+stairs= pyglet.image.load('assets/forest-assets/stairs.png')
+door = pyglet.image.load('assets/forest-assets/door.png')
+font = pygame.font.SysFont("arial", 72)
+
+text = font.render("Hello, World", True, (0, 128, 0))
+
+
+
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
+    image.blit(100,150)
+    cavimg.blit(200,250)
+    floor.blit(300,200)
+    stairs.blit(400,100)
+    door.blit(500,100)
+    win.blit(text, (400, 300))
+    
+  
+
 
 pyglet.app.run()
